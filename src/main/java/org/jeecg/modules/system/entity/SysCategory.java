375f8be874f8ccd674646b63f21dba579dfdf995
tanpeng <tanpeng@yadingdata.com>
2020-08-20 11:02:15 +0800

    system包

---
+++ src/main/java/org/jeecg/modules/system/entity/SysCategory.java
@@ -0,0 +1,69 @@
+package org.jeecg.modules.system.entity;
+
+import java.io.Serializable;
+
+import org.apache.commons.lang.StringUtils;
+import org.jeecgframework.poi.excel.annotation.Excel;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import lombok.Data;
+
+/**
+ * @Description: 分类字典
+ * @Author: jeecg-boot
+ * @Date:   2019-05-29
+ * @Version: V1.0
+ */
+@Data
+@TableName("sys_category")
+public class SysCategory implements Serializable,Comparable<SysCategory>{
+    private static final long serialVersionUID = 1L;
+
+	/**主键*/
+	@TableId(type = IdType.ID_WORKER_STR)
+	private String id;
+	/**父级节点*/
+	private String pid;
+	/**类型名称*/
+	@Excel(name = "类型名称", width = 15)
+	private String name;
+	/**类型编码*/
+	@Excel(name = "类型编码", width = 15)
+	private String code;
+	/**创建人*/
+	private String createBy;
+	/**创建日期*/
+	@JsonFormat(timezone = "GMT+8",pattern = "yyyy-MM-dd HH:mm:ss")
+    @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")
+	private java.util.Date createTime;
+	/**更新人*/
+	private String updateBy;
+	/**更新日期*/
+	@JsonFormat(timezone = "GMT+8",pattern = "yyyy-MM-dd HH:mm:ss")
+    @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")
+	private java.util.Date updateTime;
+	/**所属部门*/
+	private String sysOrgCode;
+	/**是否有子节点*/
+	@Excel(name = "是否有子节点(1:有)", width = 15)
+	private String hasChild;
+
+	@Override
+	public int compareTo(SysCategory o) {
+		//比较条件我们定的是按照code的长度升序
+		// <0：当前对象比传入对象小。
+		// =0：当前对象等于传入对象。
+		// >0：当前对象比传入对象大。
+		int	 s = this.code.length() - o.code.length();
+		return s;
+	}
+	@Override
+	public String toString() {
+		return "SysCategory [code=" + code + ", name=" + name + "]";
+	}
+}
